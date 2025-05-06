68ea2cb92e7a343a42593ec6c46978e537d3ad60
Tlhalefang Ntshilane <Tlhalefangntshilane@gmail.com>
2025-05-06 17:10:13 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -100,7 +100,7 @@ const Footer = () => {
         </div>
         
         <div className="border-t border-gray-800 mt-12 pt-8 text-center">
-          <p className="text-gray-400">© {new Date().getFullYear()} Get More BW. All rights reserved.</p>
+          <p className="text-gray-400">© {new Date().getFullYear()} Get More BW. All rights reserved. Built by Tlhalefang Ntshilane</p>
         </div>
       </div>
     </footer>
